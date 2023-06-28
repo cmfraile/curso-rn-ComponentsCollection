acb875c59c16f2202d9e6ab4a122bc8ff3381468
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-06-28 18:28:45 +0200

    196

--- src/screens/detailScreens/animacion1.screen.tsx
+++ src/screens/detailScreens/animacion1.screen.tsx
@@ -1,35 +1,35 @@
 import { useEffect, useRef } from "react";
 import { Animated, Button, Dimensions, StyleSheet, Text , TouchableOpacity, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Easing } from "react-native";
 
 const { width } = Dimensions.get('window')
 
 const Animation1 = () => {
 
     const opacity = useRef( new Animated.Value(0.5) ).current ;
+    const top = useRef( new Animated.Value(50) ).current ;
 
     const fades = {
 
-        fadeIn:() => 
-        Animated.timing(
-            opacity,
-            {toValue:1,duration:2500,useNativeDriver:true}
-        ).start(({finished}) => {console.log(finished)}),
+        fadeIn:() => {
+            Animated.timing(opacity,{toValue:0.75,duration:2500,useNativeDriver:true}).start(({finished}) => {console.log(finished)});
+            Animated.timing(top,{toValue:0,duration:1000,easing:Easing.bounce,useNativeDriver:true}).start(({finished}) => {console.log(finished)})
+        },
 
-        fadeOut:() => 
-        Animated.timing(
-            opacity,
-            {toValue:0,duration:2500,useNativeDriver:true}
-        ).start(({finished}) => {console.log(finished)})
+        fadeOut:() => {
+            Animated.timing(opacity,{toValue:0.25,duration:2500,useNativeDriver:true}).start(({finished}) => {console.log(finished)});
+            Animated.timing(top,{toValue:100,duration:1000,easing:Easing.bounce,useNativeDriver:true}).start(({finished}) => {console.log(finished)})
+        }
 
     }
 
     return(
-        <SafeAreaView style={styles.container}>
+        <Animated.View style={[styles.container,{transform:[{translateY:top}]}]}>
             <Animated.View style={{width:width/3,height:width/3,backgroundColor:'purple',borderRadius:10,opacity}}></Animated.View>
             <TouchableOpacity style={styles.button} onPress={() => fades.fadeIn()}><Text>IN</Text></TouchableOpacity>
             <TouchableOpacity style={styles.button} onPress={() => fades.fadeOut()}><Text>OUT</Text></TouchableOpacity>
-        </SafeAreaView>
+        </Animated.View>
     )
 
 }
